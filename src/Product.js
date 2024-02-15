76806d6c12ad847f2917d0504bc820b104538779
Qiroo <valdecanasjustin@gmail.com>
2024-02-15 13:22:45 +0800

    test

--- src/Product.js
+++ src/Product.js
@@ -1,12 +1,21 @@
 function Product(props) {
-  const { name, type, price, formerPrice, image } = props.data;
+  const { name, type, price, formerPrice, quantity, favorite, image } =
+    props.data;
 
   return (
     <div className="col-md-3">
       <div className="product-card">
         <div className="product-card-img">
-          <label className="stock bg-success">In Stock</label>
-          <img src={image} alt="Laptop" />
+          {quantity === 0 ? (
+            <label className="stock bg-danger">Out of Stock</label>
+          ) : (
+            <label className="stock bg-success">In Stock</label>
+          )}
+          <img
+            src={image}
+            alt="Laptop"
+            className={`${quantity === 0 ? "sold-out" : ""}`}
+          />
         </div>
         <div className="product-card-body">
           <p className="product-brand">{type}</p>
@@ -17,13 +26,23 @@ function Product(props) {
             <span className="selling-price">${price}</span>
             <span className="original-price">${formerPrice}</span>
           </div>
+          <p>Stock: {quantity}</p>
           <div className="mt-2">
+            {quantity === 0 ? (
+              <a href="" className="btn btn1" style={{ color: "red" }} disabled>
+                SOLD OUT
+              </a>
+            ) : (
+              <a href="" className="btn btn1">
+                Add To Cart
+              </a>
+            )}
             <a href="" className="btn btn1">
-              Add To Cart
-            </a>
-            <a href="" className="btn btn1">
-              {" "}
-              <i className="fa fa-heart"></i>{" "}
+              {favorite === true ? (
+                <i className="fa fa-heart" style={{ color: "red" }}></i>
+              ) : (
+                <i className="fa fa-heart"></i>
+              )}
             </a>
             <a href="" className="btn btn1">
               {" "}
